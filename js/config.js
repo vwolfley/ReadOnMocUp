3379157b600aa67cc4ae36e489a79d0714c753fb
Vern Wolfley <vern.wolfley@gmail.com>
2014-12-02 23:10:34 -0700

    code updates

---
+++ js/config.js
@@ -0,0 +1,25 @@
+var appConfig = new function() {
+
+    this.schoolData = [{
+        school: "Litchfield",
+        type: "public",
+        gradeLevel: "KG-8",
+        district: "Litchfield Elementary",
+        titleI: "No",
+        letterGrade: "A",
+        freeReduced: "",
+        totalEnroll: "",
+        k3Enroll: ""
+    }, {
+        school: "Bonita",
+        type: "public",
+        gradeLevel: "KG-8",
+        district: "Bonita Elementary",
+        titleI: "Yes",
+        letterGrade: "B",
+        freeReduced: "",
+        totalEnroll: "",
+        k3Enroll: ""
+    }];
+
+};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+    var window = $("#window"),
+        undo = $("#undo")
+        .bind("click", function() {
+            window.data("kendoWindow").open();
+            undo.hide();
+        });
+
+    var onClose = function() {
+        undo.show();
+    }
+
+    if (!window.data("kendoWindow")) {
+        window.kendoWindow({
+            width: "660px",
+            title: "ReadOn School Data",
+            actions: [
+                "Minimize",
+                "Close"
+            ],
+            close: onClose
+        });
+    }
+
     $("#tabstrip").kendoTabStrip({
         animation: {
             open: {
@@ -7,3 +30,52 @@ $(document).ready(function() {
         }
     });
 });
+
+function createChart() {
+    $("#chart").kendoChart({
+        title: {
+            text: "Enrollment"
+        },
+        legend: {
+            position: "top"
+        },
+        seriesDefaults: {
+            type: "column"
+        },
+        series: [{
+            name: "Total Enrollment",
+            data: [1090, 1085, 995, 1065]
+        }, {
+            name: "K-3 Enrollment",
+            data: [465, 455, 392, 460]
+        }],
+        valueAxis: {
+            labels: {
+                format: "{0:N0}"
+            },
+            line: {
+                visible: false
+            },
+            axisCrossingValue: 0
+        },
+        categoryAxis: {
+            categories: [2010, 2011, 2012, 2013],
+            line: {
+                visible: false
+            },
+            labels: {
+                padding: {
+                    top: 135
+                }
+            }
+        },
+        tooltip: {
+            visible: true,
+            format: "N0",
+            template: "#= series.name #: #= value #"
+        }
+    });
+}
+
+$(document).ready(createChart);
+$(document).bind("kendo:skinChange", createChart);
